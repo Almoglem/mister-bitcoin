79b980d56b915be3dfabb46932bffad121866be9
Almog <almoglemberg@gmail.com>
2021-04-09 16:18:56 +0300

    charts changed to chartJS

--- src/cmps/Charts/Charts.jsx
+++ src/cmps/Charts/Charts.jsx
@@ -1,43 +1,37 @@
-
-// import { Line } from 'react-chartjs-2';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
-
+import { Line } from 'react-chartjs-2';
 import './Charts.scss'
 
-export function Charts({ marketPriceVals, tradeVolumeVals }) {
+export function Charts({ marketPriceData, tradeVolumeData }) {
     return (
         <section>
+            { marketPriceData &&
+                <div className="chart-container">
+                    <Line
+                        data={{
+                            labels: marketPriceData.labels,
+                            datasets: [{
+                                label: 'Market Price',
+                                data: marketPriceData.vals
+                            }]
+                        }}
 
-            {marketPriceVals &&
-                <div>
-                    <h1>Market Price Stats</h1>
-                    <Sparklines data={marketPriceVals} width={100} height={20} margin={3}>
-                        <SparklinesLine color='black' />
-                    </Sparklines>
-                </div>}
-
-
-            {tradeVolumeVals &&
-                <div>
-                    <h1>Trade Volume Stats</h1>
-                    <Sparklines data={tradeVolumeVals} width={100} height={20} margin={3}>
-                        <SparklinesLine color='black' />
-                    </Sparklines>
-                </div>}
-
-            {/* <Line
-                data={{
-                    labels: ['hihihhi', 'hihi'],
-                    datasets: [{
-                        label: 'blah',
-                        data: [1, 2]
-                    }]
-                }}
-                height={400}
-                width={400}
-            /> */}
-
-        </section>
+                    />
+                </div>
+            }
+            { tradeVolumeData &&
+                <div className="chart-container">
+                    <Line
+                        data={{
+                            labels: tradeVolumeData.labels,
+                            datasets: [{
+                                label: 'Trade Volume',
+                                data: tradeVolumeData.vals
+                            }]
+                        }}
+                    />
+                </div>
+            }
+        </section >
     )
 }
 
--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -2,32 +2,42 @@
 import { Component } from 'react'
 import { bitcoinService } from '../../services/bitcoinService'
 import { Charts } from '../../cmps/Charts'
+import moment from 'moment'
 import './Statistics.scss'
 
 export class Statistics extends Component {
     state = {
-        marketPriceVals: null,
-        tradeVolumeVals: null
+        marketPriceData: null,
+        tradeVolumeData: null
     }
     componentDidMount() {
-        this.loadData();
+        this.loadMarketPlaceData();
+        this.LoadTradeVolumeData();
     }
-    async loadData() {
-        const marketPriceData = await bitcoinService.getMarketPrice();
-        const marketPriceVals = marketPriceData.values.map(value => value.y);
-        this.setState({ marketPriceVals })
 
-        const tradeVolumeData = await bitcoinService.getTradeVolume();
-        const tradeVolumeVals = tradeVolumeData.values.map(value => value.y);
-        this.setState({ tradeVolumeVals })
+
+    async loadMarketPlaceData() {
+        const data = await bitcoinService.getMarketPrice();
+        const marketPriceData = {};
+        marketPriceData.vals = data.values.map(coord => coord.y);
+        marketPriceData.labels = data.values.map(coord => moment.unix(coord.x).format("DD/MM/YYYY"))
+        this.setState({ marketPriceData })
     }
+    async LoadTradeVolumeData() {
+        const data = await bitcoinService.getTradeVolume();
+        const tradeVolumeData = {};
+        tradeVolumeData.vals = data.values.map(coord => coord.y);
+        tradeVolumeData.labels = data.values.map(coord => moment.unix(coord.x).format("DD/MM/YYYY"))
+        this.setState({ tradeVolumeData })
+    }
+
     render() {
-        const { marketPriceVals, tradeVolumeVals } = this.state
+        const { marketPriceData, tradeVolumeData } = this.state
         return (
             <div>
                 <h3>Statistics</h3>
-                <Charts marketPriceVals={marketPriceVals}
-                    tradeVolumeVals={tradeVolumeVals}
+                <Charts marketPriceData={marketPriceData}
+                    tradeVolumeData={tradeVolumeData}
                 />
             </div>
         )
